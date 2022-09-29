60275cfc4eee90d0233a4b83f103c2a66435fb59
João Felipe <87448021+Felipe-JF@users.noreply.github.com>
2022-09-29 11:40:37 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 type Element = {
   tag: string;
+  id?: string;
+  data?: Element["id"];
   children?: readonly Element[];
 };
 
-function Element<T extends Element["tag"], E extends Element & { tag: T }>(
-  tag: T,
-  element: Omit<E, "tag">
-): E {
-  return <E>{
-    ...element,
-    tag,
-  };
+function Element<E extends Element>(element: E): E {
+  return element;
 }
 
 function generateXmlElement(element: Element): string {
@@ -21,6 +17,56 @@ function generateXmlElement(element: Element): string {
   return `<${element.tag}>
   ${element.children.map(generateXmlElement)}
 </${element.tag}>`;
+}
+
+function collectIds(
+  element: Element,
+  references: Map<Element["id"], Element>
+): Map<Element["id"], Element> {
+  if (element.id) {
+    references.set(element.id, element);
+  }
+
+  if (element.children) {
+    const map = element.children.reduce(
+      (state, children) => collectIds(children, state),
+      references
+    );
+
+    return map;
+  }
+
+  return references;
+}
+
+function validateReferences(
+  element: Element,
+  ids: Map<Element["id"], Element>
+): Map<Element["id"], Element> {
+  if (element.data) {
+    const reference = ids.get(element.data);
+    if (!reference) {
+      throw new Error(
+        "Reference " +
+          element.data +
+          " in element " +
+          generateXmlElement(element) +
+          " does not exists"
+      );
+    }
+    return ids;
+  }
+
+  if (element.children) {
+    const map = element.children.reduce(
+      (state, children) => validateReferences(children, state),
+      references
+    );
+
+    return map;
+  }
+
+  return ids;
 }
 
 function validate(element: Element, tags: readonly string[]) {
@@ -44,22 +90,39 @@ function validate(element: Element, tags: readonly string[]) {
   const childrenInvalid = element.children?.some(
     (element) => !validate(element, tags)
   );
+
   return !childrenInvalid;
 }
 
-const element = Element("hello", {
+const element: Element = {
+  tag: "hello",
   children: [
-    Element("Alo", {
+    {
+      tag: "hello",
+      id: "referencia importante",
       children: [
-        Element("hello", {
-          children: [Element("Alo", { children: [] })],
-        }),
+        {
+          tag: "hello",
+          id: "sub referencia importante",
+          children: [],
+        },
       ],
-    }),
+    },
+    {
+      tag: "hell",
+      data: "sub referencia importante",
+      children: [],
+    },
   ],
-});
+};
 
 const validTags = ["hello", "alo"] as const;
+
+const references = collectIds(element, new Map());
+
+console.log({ references });
+
+validateReferences(element, references);
 
 const isValid = validate(element, validTags);
 
